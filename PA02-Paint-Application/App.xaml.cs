b0ebfd089df8fe2c89231feb9edd91186428e1d7
PICKLE\hoang <21127092@student.hcmus.edu.vn>
2024-04-13 22:28:13 +0700

    Add project files.

---
+++ PA02-Paint-Application/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PA02_Paint_Application
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
